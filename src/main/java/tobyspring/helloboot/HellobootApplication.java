e586be1f63ad903b56d216c6d5d9c21f62d47e11
tintoll <belselios@gmail.com>
2023-02-23 06:35:12 +0900

    인프라스트럭쳐 빈들 패키지분리

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,17 +1,11 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MySpringBootApplication;
 
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
-
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
