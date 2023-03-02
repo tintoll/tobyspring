1b59904a1a430178e370b3bb3d793dac4bae9feb
tintoll <belselios@gmail.com>
2023-03-02 15:13:47 +0900

    import 항목 파일에서 가져오도록 변경

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,29 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 // ImportSelect 에서 동적으로 빈을 만들어줄 클래스들을 가져올수 있다.
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig",
-        };
+
+        List<String> autoConfigs = new ArrayList<>();
+        // 파일에서 해당 정보를 읽어오는 부분으로 구현
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
