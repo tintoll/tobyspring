1d79ab7f4deb76bd6b09d5ef0cf221628261c6d5
tintoll <belselios@gmail.com>
2023-03-07 22:06:04 +0900

    @Value를 이용한 환경변수 가져오기

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -16,7 +16,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res = rest
-                .getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+                .getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         assertThat(res.getBody()).isEqualTo("*Hello Spring*");
@@ -28,7 +28,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res = rest
-                .getForEntity("http://localhost:8080/hello", String.class);
+                .getForEntity("http://localhost:8080/app/hello", String.class);
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
